f40115e51a3d646963efb513d7b9ca4a8076dae3
melni <melnikovalmaz@gmail.com>
2018-04-17 02:42:44 +0300

    Add requirements.txt Add description to README.MD

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# bank_assistant
-Bank assistant is project that automaticaly answer to clients questions.
+# Bank Assistant
+Bank assistant is project that automatically provide possible answers to client's questions.
+It is implemented in Telegram bot format. Username of bot is **@Sberbank_assistant_testbot**
+## Installation
+Clone project from GitHub repository.
+
+Install requirement packages by pip:
+
+**pip install -r requirements.txt**
+
+### Start project
+Use command:
+
+**python telegram_bot.py**
+
+## Project structure
+Project consists of 3 parts:
+1. Telegram bot
+2. Assistant
+3. Model training
+
+Telegram bot is simple, just realize one function, accept message and return answer.
+
+Assistant realize 2 function:
+1. Find 5 nearest clusters and return information in format {key_words}, {confidence}.
+Method name is **_get_nearest_clusters(self, query, num_clusters)_**
+
+2. Find 5 similar questions and return information in format {question}, {answer}, {confidence}.
+Method name is **_get_nearest_questions(self, query, num_clusters)_**
+
+### Model training
+Model pipeline structure is:
+1. Load data
+2. Preprocess data and filter
+3. Convert documents to vectors by TF-IDF vectorizer
+4. Find clusters by KMeans algorithm
+
+#### Preprocess and filter
+Preprocessing operations are implemented in _assistant/training/preprocess.py_ module.
+There are used lemmatization ,removing stopwords, converting vk links to word "vklink".
--- assistant/training/preprocess/preprocess.py
+++ assistant/training/preprocess/preprocess.py
@@ -3,6 +3,7 @@ import re
 from nltk.tokenize import TweetTokenizer
 import nltk
 
+nltk.download('stopwords')  # Download stopwords
 
 morph = pm.MorphAnalyzer()
 tokenizer = TweetTokenizer()
@@ -11,37 +12,42 @@ tokenizer = TweetTokenizer()
 vk_regexp = r"(https?:\/\/)?(www\.)?(new\.)?(m\.)?vk\.com\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
 vklink = "vklink"
 
+# List of gramems that is allowed. From pymorphy2 library
 gramem_list = ['NOUN', "ADJF", "ADJS", "VERB", "INFN", "PRTF", "PRTS", "GRND"]
 
+# Prepare stopwords
 stopwords = nltk.corpus.stopwords.words('russian')
-stopwords += ["мочь", "пожалуйста", "сбербанк", "быть", "банк", "банка"]
+stopwords += ["мочь", "пожалуйста", "сбербанк", "быть", "банк", "банка"]  # Custom
 
 
 # Filters
 def filter_question(question):
     is_accepted = True
-    is_accepted = is_accepted and len(question.split()) > 2
-    is_accepted = is_accepted and len(question) > 20
+    is_accepted = is_accepted and len(question.split()) > 2 # More than 2 words in question
+    is_accepted = is_accepted and len(question) > 20  # Length of question more than 20 letters
     return is_accepted
 
 
-# Lemmatization
+# Lemmatize
 def lemmatize_strings(string_list):
-    new_list = [morph.parse(string)[0].normal_form for string in string_list if (morph.parse(string)[0].tag.POS in gramem_list or string == vklink) and morph.parse(string)[0].normal_form not in stopwords]
+    new_list = [morph.parse(string)[0].normal_form for string in string_list if
+                (morph.parse(string)[0].tag.POS in gramem_list or string == vklink) and morph.parse(string)[
+                    0].normal_form not in stopwords]
     return new_list
 
 
 # Preprocessing
 def preprocess_question(question):
-    question = question.lower() # convert to lower case
-    question = re.sub(vk_regexp, vklink, question) # convert vk links to special word
-    question = tokenizer.tokenize(question) # tokenize string
-    question = lemmatize_strings(question) # lemmatize question
+    question = question.lower()  # convert to lower case
+    question = re.sub(vk_regexp, vklink, question)  # convert vk links to special word
+    question = tokenizer.tokenize(question)  # tokenize string
+    question = lemmatize_strings(question)  # lemmatize question
     new_question = ""
     for string in question:
         new_question += string + " "
     new_question = new_question.strip()
     return new_question
+
 
 if __name__ == "__main__":
     print(preprocess_question("https://vk.com/id393445899 - злой клон почему админа группы = мошенник!!!!!!!!!!"))
